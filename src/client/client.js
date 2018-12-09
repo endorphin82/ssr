0505957f569f4756c4420f4e230b489b349c6c93
endorphin82 <endorphin82@gmail.com>
2018-12-09 06:13:19 +0200

    05 Adding Navigation

--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,12 @@
 // Start point for client side application
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/Home';
- 
-ReactDOM.hydrate(<Home />, document.querySelector('#root'));
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './Routes';
+
+ReactDOM.hydrate(
+  <Router>
+    <Routes />
+  </Router>
+  , document.querySelector('#root')
+);
